aac6c9cea0c7d02430efef003e59feef86edda4b
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-22 12:20:37 +1300

    did date

---
+++ ictgradschool/web/lab06/ex05/displaytime.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    var d = new Date();
+    // function myFunction() {
+    //     document.getElementById("demo").innerHTML=d;
+    // }
+
+document.getElementById("datetime").innerText=d;
+}
