72f54f39c9f3c8a028d85ef713c8220fb177e617
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-22 13:38:21 +1300

    done ex06

--- ictgradschool/web/lab06/ex06/domtraverse.js
+++ ictgradschool/web/lab06/ex06/domtraverse.js
@@ -1,1 +1,19 @@
 /* Your answer here */
+function pinkify() {
+    document.getElementsByClassName("subtitle")[0].textContent ="Hello World";
+    document.getElementsByClassName("text2")[0].style.display="none";
+document.getElementsByClassName("text1")[0].style.color ="pink";
+document.getElementById("footer").style.color="red";
+
+}
+
+function boldy(){
+    var par=document.getElementsByTagName("p");
+    for(var i=0;i<par.length;i++){
+        if(par[i].style.fontWeight===""){
+            par[i].style.fontWeight = "bold";
+        } else {
+            par[i].style.fontWeight = "";
+        }
+    }
+}
